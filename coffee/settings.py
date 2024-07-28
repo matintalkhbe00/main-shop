e9c86afbfc8394b87b323ac43189eaa49b9a1c53
kmmatin66 <65290307+kmmatin66@users.noreply.github.com>
2024-07-28 21:36:25 +0330

    homepage

--- coffee/settings.py
+++ coffee/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # app
     'account_app',
-    'home_app'
+    'home_app',
+
+
 ]
 
 MIDDLEWARE = [
--- home_app/views.py
+++ home_app/views.py
@@ -4,4 +4,4 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "home_app/index.html"
+    template_name = "home_app/home.html"
